de6ea15b9e877755d2c7df23833337ea9a0b6680
PatrickJMcArdle <patrickjmcardle1@gmail.com>
2025-08-05 12:02:39 -0500

    finished queries

--- db/queries/faculties.js
+++ db/queries/faculties.js
@@ -9,10 +9,58 @@ export async function getFaculties() {
   return faculties;
 }
 
-export async function getFacultyById() {}
+export async function getFacultyById(id) {
+  const sql = `
+    SELECT *
+    FROM faculty
+    WHERE id = $1
+    `;
+  const {
+    rows: [faculty],
+  } = await db.query(sql, [id]);
+  return faculty;
+}
 
-export async function createFaculty() {}
+export async function updateFaculty(
+  id,
+  name,
+  title,
+  sub_department,
+  profile_img,
+  bio,
+  email,
+  department_id
+) {
+  const sql = `
+  UPDATE faculties
+  SET
+    name = $2
+    title = $3
+    sub_department = $4
+    profile_img = $5
+    bio = $6
+    email = $7
+    department_id = $8
+  WHERE id = $1
+  RETURNING *
+  `;
+  const {
+    rows: [faculty],
+  } = await db.query(sql, [id,
+  name,
+  title,
+  sub_department,
+  profile_img,
+  bio,
+  email,
+  department_id]);
+  return faculty;
+}
 
-export async function updateFaculty() {}
-
-export async function deleteFaculty() {}
+export async function deleteFaculty(id) {
+  const sql = `
+    DELETE FROM faculties
+    WHERE id = $1
+    `;
+  await db.query(sql, [id]);
+}
