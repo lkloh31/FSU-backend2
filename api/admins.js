9feb41b408439fcb17eddb43860bd31a7d08480c
Devin <devinrsantillanes@gmail.com>
2025-08-05 20:33:50 -0600

    changed departmentrouter getdepartmentbyid

--- api/admins.js
+++ api/admins.js
@@ -4,25 +4,28 @@ const adminRouter = express.Router();
 export default adminRouter;
 
 import { createToken } from "#utils/jwt";
-import { createAdmin, getAdminByUsernameAndPassword } from "#db/queries/administrators";
+import {
+  createAdmin,
+  getAdminByUsernameAndPassword,
+} from "#db/queries/administrators";
 
 adminRouter
-    .route("/register")
-    .post(requireBody(["name", "username", "password"]), async (req, res) => {
-        const { name, username, password } = req.body;
-        const admin = await createAdmin(name, username, password);
+  .route("/register")
+  .post(requireBody(["name", "username", "password"]), async (req, res) => {
+    const { name, username, password } = req.body;
+    const admin = await createAdmin(name, username, password);
 
-        const token = createToken({ id: admin.id });
-        res.status(201).send(token);
-    });
+    const token = createToken({ id: admin.id });
+    res.status(201).json(token);
+  });
 
 adminRouter
-    .route("/login")
-    .post(requireBody(["username", "password"]), async (req, res) => {
-        const { username, password } = req.body;
-        const admin = await getAdminByUsernameAndPassword(username, password);
-        if (!admin) return res.status(401).send("Invalid username or password.");
+  .route("/login")
+  .post(requireBody(["username", "password"]), async (req, res) => {
+    const { username, password } = req.body;
+    const admin = await getAdminByUsernameAndPassword(username, password);
+    if (!admin) return res.status(401).send("Invalid username or password.");
 
-        const token = createToken({ id: admin.id });
-        res.send(token);
-    })
+    const token = await createToken({ id: admin.id });
+    res.json(token);
+  });
--- api/departments.js
+++ api/departments.js
@@ -4,22 +4,32 @@ import express from "express";
 const deptRouter = express.Router();
 export default deptRouter;
 
-import { getDepartments, getDepartmentById, updateDepartmentById, deleteDepartmentById } from "#db/queries/departments";
+import {
+  getDepartments,
+  getDepartmentById,
+  updateDepartmentById,
+  deleteDepartmentById,
+} from "#db/queries/departments";
 
-deptRouter.route("/").get(async (req, res) => {
+deptRouter
+  .route("/")
+  .get(async (req, res) => {
     const departments = await getDepartments();
-    res.send(departments)
-})
-.post(requireUser, requireBody(["name", "description", "banner_img"]), async (req, res) => {
-    const { name, description, banner_img } = req.body;
-    const department = await createDepartment({
+    res.send(departments);
+  })
+  .post(
+    requireUser,
+    requireBody(["name", "description", "banner_img"]),
+    async (req, res) => {
+      const { name, description, banner_img } = req.body;
+      const department = await createDepartment({
         name,
         description,
-        banner_img
-    });
-    res.status(201).send(department)
-})
-
+        banner_img,
+      });
+      res.status(201).send(department);
+    }
+  );
 
 deptRouter.param("id", async (req, res, next, id) => {
   if (!/^\d+$/.test(id)) {
@@ -32,22 +42,28 @@ deptRouter.param("id", async (req, res, next, id) => {
   next();
 });
 
-deptRouter.route("/:id/:name").get(async (req, res) => {
-   res.send(req.department);
-})
-.put(requireUser, requireBody(["name", "description", "banner_img"]), async (req, res) => {
-    const { id } = req.department.id
-    const { name, description, banner_img } = req.body;
-    const department = await updateDepartmentById({
+deptRouter
+  .route("/:id")
+  .get(async (req, res) => {
+    res.send(req.department);
+  })
+  .put(
+    requireUser,
+    requireBody(["name", "description", "banner_img"]),
+    async (req, res) => {
+      const { id } = req.department.id;
+      const { name, description, banner_img } = req.body;
+      const department = await updateDepartmentById({
         id,
         name,
         description,
-        banner_img
-    })
-    res.send(department)
-})
-.delete(requireUser, async (req, res) => {
-    const {id} = req.department.id;
-    const department = deleteDepartmentById(id)
-    res.sendStatus(204)
-})
+        banner_img,
+      });
+      res.send(department);
+    }
+  )
+  .delete(requireUser, async (req, res) => {
+    const { id } = req.department.id;
+    const department = deleteDepartmentById(id);
+    res.sendStatus(204);
+  });
